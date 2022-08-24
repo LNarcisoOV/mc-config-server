7c90e88f1d361818aeb4743c7f56dccdd8a480cf
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-08-24 13:06:11 +0000

    First commit.

---
+++ src/main/java/com/mc/address/McConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mc.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/address/McConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
